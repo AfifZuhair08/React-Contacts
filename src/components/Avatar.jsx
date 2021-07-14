5f8f2ecefb5b938df85a89707d69fb05567dd369
Afif Zuhair <afifzuhair08@gmail.com>
2021-07-14 23:30:19 +0800

    Create initial react export, import compnents and using props to transfer data

---
+++ src/components/Avatar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Avatar(props) {
+    return(
+        <img className="circle-img"
+        src={props.avatarImg}
+        alt="avatar_img"
+      />
+    );
+}
+
+export default Avatar;
